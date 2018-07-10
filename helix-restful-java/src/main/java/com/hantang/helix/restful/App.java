6a05f978fc4feb8f6d361db127a633556c118747
Allen Wang <allenthinking@outlook.com>
2018-07-10 15:51:48 +0800

    initialize the Java Maven Project

---
+++ helix-restful-java/src/main/java/com/hantang/helix/restful/App.java
@@ -0,0 +1,13 @@
+package com.hantang.helix.restful;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
