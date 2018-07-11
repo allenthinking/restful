7f9b966e2c7c172f198e0bc1e2a3caa2460d80a8
Allen Wang <allenthinking@outlook.com>
2018-07-11 19:32:44 +0800

    initialize the Java Maven Project

---
+++ helix-jdk8/src/main/java/com/hantang/helix/jdk1/helix_jdk8/App.java
@@ -0,0 +1,13 @@
+package com.hantang.helix.jdk1.helix_jdk8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
